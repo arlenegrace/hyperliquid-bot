ffa77a37a52b71a863aba257b94a9e040b50100f
arlenegrace <63443490+arlenegrace@users.noreply.github.com>
2026-03-28 06:24:23 -0700

    Add back initial manual range trading strategy (does not have risk guardrails set up). For comparison during backtests only

--- strategies/index.ts
+++ strategies/index.ts
@@ -1,11 +1,13 @@
 import type { TradingStrategy } from "../src/types.js";
 import { AnchoredRangeLadderStrategy } from "./anchoredRangeLadder.js";
 import { ManualRangeTradingStrategy } from "./manualRangeTrading.js";
+import { ManualRangeTradingV1Strategy } from "./manualRangeTradingV1.js";
 import { PivotClusterMeanReversionStrategy } from "./pivotClusterMeanReversion.js";
 
 export function createAllStrategies(): TradingStrategy[] {
   return [
     new ManualRangeTradingStrategy(),
+    new ManualRangeTradingV1Strategy(),
     new AnchoredRangeLadderStrategy(),
     new PivotClusterMeanReversionStrategy(),
   ];
